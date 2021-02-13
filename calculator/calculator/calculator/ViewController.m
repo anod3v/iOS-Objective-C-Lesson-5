848ae21ede811c79078c148caa586e4cf462cb57
Anoshkin <a.anoshkin@profsoft.pro>
2021-02-13 14:31:36 +0300

    calc added

--- calculator/calculator/calculator/ViewController.m
+++ calculator/calculator/calculator/ViewController.m
@@ -13,9 +13,39 @@
 
 @implementation ViewController
 
+enum Operation {
+    Sum,
+    Subtract,
+    Multiply,
+    Divide
+};
+
+typedef enum Operation Operation;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    CGFloat result = calculate(1, 3, Divide);
+    NSLog(@"the result is: %f", result);
+    
+}
+
+CGFloat calculate(CGFloat a, CGFloat b, Operation operation) {
+    
+    switch (operation) {
+        case Sum:
+            return a + b;
+            break;
+        case Subtract:
+            return a - b;
+            break;
+        case Divide:
+            return a / b;
+            break;
+        case Multiply:
+            return a * b;
+            break;
+            
+    }
 }
 
 
